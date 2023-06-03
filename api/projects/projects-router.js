7ab2ff3ec7376908c75d07978f9f2adb75acc0a5
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-06-03 15:31:33 -0400

    setting up the routs

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,14 @@
 // Write your "projects" router here!
+const router = require('express').Router()
+const projects = require('./projects-model')
+const projectsmw = require('./projects-middleware')
+
+
+router.get( '/')
+
+
+
+
+
+
+module.exports = router
